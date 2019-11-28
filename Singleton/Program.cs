fdcc2ac8e327ffe1556f7cd387fada6d7dfea279
dotnet111119 <57640157+dotnet111119@users.noreply.github.com>
2019-11-28 19:17:57 +0200

    Add files via upload

---
+++ Singleton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2811
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+
+            // will generate same reference
+            //Singleton s2 = Singleton.GetInstance();
+
+            // will return true
+            //Console.WriteLine(s1 == s2);
+
+            // execute command from the singleton class...
+            s1.GetConnection();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2811
+{
+    public class Singleton
+    {
+        // 1
+        private static Singleton INSTANCE;
+
+        List<DbConnections> dbConnections;
+
+        // 2 private
+        private Singleton()
+        {
+
+        }
+
+        // 3 - get instance
+        public static Singleton GetInstance()
+        {
+            if (INSTANCE == null)
+            {
+                INSTANCE = new Singleton();
+            }
+            return INSTANCE;
+        }
+
+        public DbConnections GetConnection()
+        {
+            // check if there are free connections ...
+            DbConnections conn = dbConnections[0];
+            dbConnections.RemoveAt(0);
+            return conn;
+
+        }
+        public void RetutnConnection(DbConnections db)
+        {
+            dbConnections.Add(db);
+        }
+    }
+}
